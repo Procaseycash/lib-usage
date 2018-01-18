77458d387ed8c9f2047004e3144a77ce720b64d3
KazeemO <kezyolanipekun@gmail.com>
2018-01-19 00:14:47 +0100

    table searcher together with paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {EventsService} from "larang-paginator";
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
-import {TableSearcherInterface, TableSearcherTypesEnum} from "angular-table-searcher";
+import {TableSearcherInterface, TableSearcherTypesEnum, EventsService as EventServe} from "angular-table-searcher";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   };
 
-  public tableSearcher: TableSearcherInterface<Object> = {
+  public tableSearcher: TableSearcherInterface<any> = {
     path: 'http://localhost:8088/api/organizations',
     searchType: TableSearcherTypesEnum.EMPTY_TABLE_APPLY_BACKEND,
     searchKeys: ['name', 'email'], // can be empty array to enable deep searching
@@ -31,20 +31,20 @@ export class AppComponent implements OnInit, AfterViewInit {
     from: 'search_organizations'
   };
 
-  constructor(private eventsService: EventsService, private http: Http) {
+  constructor(private eventsService: EventsService, private http: Http, private  eventServe: EventServe) {
     this.eventsService.on(this.paginator.from, (res) => {
       // pass response to the property rendering the data in view
 
       this.paginator.data = res.data; // update paginated data in view
     });
 
-    this.eventsService.on(this.tableSearcher.from, (res) => {
+    this.eventServe.on(this.tableSearcher.from, (res) => {
       // Note that table will response from table searcher will respond with result and data,
       // the result is the searched item while data is the previous data passed down to it.
       // the result can be of array or object.
       // if result is an array, that is the final result
       // but if result is object, it denotes a backend response, so you can drill down to pick the searched result depending on your api response.
-      // console.log('response=', res);
+      console.log('response=', res);
       this.tableSearcher.data = (res['result'].constructor === Array) ? res['result'] : res['result'].data['data']; // update table data in view
       this.paginator.data['data'] = this.tableSearcher.data;
     });
